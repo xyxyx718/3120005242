ff8e6e8ff22a50968339ceb6857deed7414a1e53
Jack83659365 <1012847171@qq.com>
2022-09-19 00:28:12 +0800

    优化了正则匹配

--- main.py
+++ main.py
@@ -17,36 +17,41 @@ def load(path):
     text = f.read()
     f.close()
 
-    # 清理空字符、html格式、引号
-    text = re.sub(r'\s', '', text)
-    text = re.sub(r'<.*?>', '', text)
-    text = re.sub(r'''[“”‘’"']''', '', text)
-    #text = re.sub(r'[a-zA-Z0-9]*', '', text)
+    # 清理html格式、引号、中文中的空格
+    text = re.sub(r'<.*?>', '', text, flags=re.S)
+    text = re.sub(r'\s{2,}', '\n', text, flags=re.S)
+    text = re.sub(r'''[“”‘’"'\(\)\{\}\[\]（）【】]''', '', text)
+    text = re.sub(r'([\n] *)[a-zA-Z0-9.,;:!?_&\*\/\-\+\=↵<>]+( *[\n])', '', text)
+    print(text)
+    text = re.sub(r'(^[a-zA-Z0-9.,!?])[ \t]+(^[a-zA-Z0-9.,!?])', '', text)
+    text = re.sub(r'(^[a-zA-Z0-9.,!?])[ \t]+(^[a-zA-Z0-9.,!?])', '', text)
+    text = re.sub(r'(^[a-zA-Z0-9.,!?])[ \t]+(^[a-zA-Z0-9.,!?])', '', text)
+    #print(text)
     return jieba.cut(text)
 
 
 def output(path, ans):
-    # 输出结果
+    # 格式化并输出结果
     f = open(r'%s' % path, mode='w', encoding='utf-8')
     f.write('%.2f' % ans)
     f.close()
 
 
-def Similarity(A_array, B_array):
+def similarity(a_array, b_array):
     # 计算两个向量夹角的余弦值
     # np.dot() 求两个一维数组的内积
     # linalg = linear（线性）+ algebra（代数），norm则表示范数
     # np.linagle.norm()即为求矩阵的二范数，即向量的模
-    A_norm = np.linalg.norm(A_array)
-    B_norm = np.linalg.norm(B_array)
-    cos_oc = A_array.dot(B_array) / (A_norm * B_norm)
+    a_norm = np.linalg.norm(a_array)
+    b_norm = np.linalg.norm(b_array)
+    cos_oc = a_array.dot(b_array) / (a_norm * b_norm)
     # 考虑到cos是非线性的，并不直观，所以将其转换为线性的角度值
     arccos_oc = np.arccos(cos_oc)/np.pi*180
     # 计算向量距离
-    dis = np.linalg.norm(A_array-B_array)
+    dis = np.linalg.norm(a_array - b_array)
     # 处理后的角度和距离的加权平均
-    similarity = ((1-arccos_oc/90)+(1-dis/(A_norm+B_norm)))/2
-    return similarity
+    sim = ((1-arccos_oc/90)+(1-dis/(a_norm+b_norm)))/2
+    return sim
 
 
 orig = load(a[1])
@@ -73,7 +78,7 @@ for item in check:
 orig_array = np.array(list(orig_dict.values()))
 check_array = np.array(list(check_dict.values()))
 
-similarity = Similarity(orig_array, check_array)
+ans = similarity(orig_array, check_array)
 
-print(similarity)
-#output(a[3], similarity)
+print(ans)
+# output(a[3], ans)
