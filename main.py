3156279a92cd6dc922449f771f8f08e68400d027
Jack83659365 <1012847171@qq.com>
2022-09-18 17:16:49 +0800

    删除多余文件 调整了相似度的计算方法

--- main.py
+++ main.py
@@ -12,10 +12,12 @@ a = sys.argv
 
 
 def load(path):
+    # 读取文件
     f = open(r'%s' % path, mode='r', encoding='utf-8')
     text = f.read()
     f.close()
 
+    # 清理空字符、html格式、引号
     text = re.sub(r'\s', '', text)
     text = re.sub(r'<.*?>', '', text)
     text = re.sub(r'''[“”‘’"']''', '', text)
@@ -24,9 +26,27 @@ def load(path):
 
 
 def output(path, ans):
+    # 输出结果
     f = open(r'%s' % path, mode='w', encoding='utf-8')
     f.write('%.2f' % ans)
     f.close()
+
+
+def Similarity(A_array, B_array):
+    # 计算两个向量夹角的余弦值
+    # np.dot() 求两个一维数组的内积
+    # linalg = linear（线性）+ algebra（代数），norm则表示范数
+    # np.linagle.norm()即为求矩阵的二范数，即向量的模
+    A_norm = np.linalg.norm(A_array)
+    B_norm = np.linalg.norm(B_array)
+    cos_oc = A_array.dot(B_array) / (A_norm * B_norm)
+    # 考虑到cos是非线性的，并不直观，所以将其转换为线性的角度值
+    arccos_oc = np.arccos(cos_oc)/np.pi*180
+    # 计算向量距离
+    dis = np.linalg.norm(A_array-B_array)
+    # 处理后的角度和距离的加权平均
+    similarity = ((1-arccos_oc/90)+(1-dis/(A_norm+B_norm)))/2
+    return similarity
 
 
 orig = load(a[1])
@@ -49,15 +69,11 @@ for item in check:
         check_dict[item] = 1
         orig_dict[item] = 0
 
-# 将字典的值转换为数组
+# 将字典的值转换为数组/多维向量
 orig_array = np.array(list(orig_dict.values()))
 check_array = np.array(list(check_dict.values()))
 
-# 计算两个多维向量的余弦值
-# np.dot() 求两个一维数组的内积
-# linalg = linear（线性）+ algebra（代数），norm则表示范数
-# np.linagle.norm()即为求矩阵的二范数，即平方和开根号
-cos_oc = orig_array.dot(check_array) / \
-    (np.linalg.norm(orig_array)*np.linalg.norm(check_array))
+similarity = Similarity(orig_array, check_array)
 
-output(a[3], cos_oc)
+print(similarity)
+#output(a[3], similarity)
